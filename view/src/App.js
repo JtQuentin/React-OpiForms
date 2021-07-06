7f8463924750edd4028ebceea57d1c7dfb23ea2f
JtQuentin <quentin.jolivet@pm.me>
2021-07-06 12:48:31 +0200

    Page login

--- view/src/App.js
+++ view/src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import login from './pages/login';  
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import login from "./pages/login";
 function App() {
   return (
-    <div>
-      <Router>
-    <div>
-       <Switch>
-           <Route exact path="/login" component={login}/>
-       </Switch>
-    </div>
-</Router>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
 export default App;
