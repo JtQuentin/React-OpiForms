4126d7548414c0ddd6f98ca6b8da0c51f2c0f2c6
JtQuentin <quentin.jolivet@pm.me>
2021-07-07 14:17:22 +0200

    delete theme

--- view/src/App.js
+++ view/src/App.js
@@ -3,33 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import login from "./pages/login";
 import signup from "./pages/signup";
 import home from "./pages/home";
-import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#33c9dc",
-      main: "#FF5722",
-      dark: "#d50000",
-      contrastText: "#fff",
-    },
-  },
-});
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-            <Route exact path="/" component={home} />
-          </Switch>
-        </div>
-      </Router>
-    </MuiThemeProvider>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+          <Route exact path="/" component={home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 export default App;
